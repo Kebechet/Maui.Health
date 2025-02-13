fa9e383dc7b0f62ae16d14957266f4a8da1c8acb
Kebechet <samosidor@gmail.com>
2025-02-13 22:14:35 +0100

    demo adjustments

---
+++ demo/DemoApp/DemoApp/Components/Pages/Home.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+using Maui.Health.Services;
+
+namespace DemoApp.Components.Pages;
+
+public partial class Home
+{
+    [Inject] public IHealthService _healthService { get; set; }
+
+    private long _steps { get; set; } = 0;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var steps = await _healthService.GetStepsTodayAsync();
+        if (steps.HasValue)
+        {
+            _steps = steps.Value;
+        }
+    }
+}
