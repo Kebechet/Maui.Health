35c8b114137cd5c84fe2878cec6be1d888ad6110
Kebechet <samosidor@gmail.com>
2025-02-14 10:02:57 +0100

    improved android stability

--- src/Maui.Health/Platforms/Android/HealthServiceAndroid.cs
+++ src/Maui.Health/Platforms/Android/HealthServiceAndroid.cs
@@ -29,78 +29,88 @@ public partial class HealthService
 
     public async partial Task<RequestPermissionResult> RequestPermissions(IList<HealthPermissionDto> healthPermissions, bool canRequestFullHistoryPermission, CancellationToken cancellationToken)
     {
-        var sdkCheckResult = IsSdkAvailable();
-        if (!sdkCheckResult.IsSuccess)
-        {
-            return new()
-            {
-                Error = RequestPermissionError.IsNotSupported
-            };
-        }
-
-        var permissionsToGrant = healthPermissions
-            .SelectMany(healthPermission => healthPermission.ToStrings())
-            .ToList();
-
-        if (canRequestFullHistoryPermission)
-        {
-            //https://developer.android.com/health-and-fitness/guides/health-connect/plan/data-types#alpha10
-            permissionsToGrant.Add("android.permission.health.READ_HEALTH_DATA_HISTORY");
-        }
-
-        var grantedPermissions = await KotlinResolver.ProcessList<Java.Lang.String>(_healthConnectClient.PermissionController.GetGrantedPermissions);
-        if (grantedPermissions is null)
-        {
-            return new()
-            {
-                Error = RequestPermissionError.ProblemWhileFetchingAlreadyGrantedPermissions
-            };
-        }
-
-        var missingPermissions = permissionsToGrant
-            .Where(permission => !grantedPermissions.ToList().Contains(permission))
-            .ToList();
-
-        if (!missingPermissions.Any())
-        {
-            return new();
-        }
-
-        var key = Guid.NewGuid().ToString();
-        var requestPermissionActivityContract = PermissionController.CreateRequestPermissionResultContract();
-        var callback = new AndroidActivityResultCallback<ISet?>(cancellationToken);
-
-        ActivityResultLauncher? launcher = null;
-        ISet? newlyGrantedPermissions = null;
         try
         {
-            var observer = ((ComponentActivity)_activityContext).ActivityResultRegistry;
-            launcher = observer.Register(key, requestPermissionActivityContract, callback);
-            launcher.Launch(new HashSet(missingPermissions));
+            var sdkCheckResult = IsSdkAvailable();
+            if (!sdkCheckResult.IsSuccess)
+            {
+                return new()
+                {
+                    Error = RequestPermissionError.IsNotSupported
+                };
+            }
 
-            newlyGrantedPermissions = await callback.Task;
-        }
-        finally
-        {
-            launcher?.Unregister();
-        }
-
-        var stillMissingPermissions = newlyGrantedPermissions is null
-            ? missingPermissions
-            : missingPermissions
-                .Where(permission => !newlyGrantedPermissions.ToList().Contains(permission))
+            var permissionsToGrant = healthPermissions
+                .SelectMany(healthPermission => healthPermission.ToStrings())
                 .ToList();
 
-        if (stillMissingPermissions.Any())
+            if (canRequestFullHistoryPermission)
+            {
+                //https://developer.android.com/health-and-fitness/guides/health-connect/plan/data-types#alpha10
+                permissionsToGrant.Add("android.permission.health.READ_HEALTH_DATA_HISTORY");
+            }
+
+            var grantedPermissions = await KotlinResolver.ProcessList<Java.Lang.String>(_healthConnectClient.PermissionController.GetGrantedPermissions);
+            if (grantedPermissions is null)
+            {
+                return new()
+                {
+                    Error = RequestPermissionError.ProblemWhileFetchingAlreadyGrantedPermissions
+                };
+            }
+
+            var missingPermissions = permissionsToGrant
+                .Where(permission => !grantedPermissions.ToList().Contains(permission))
+                .ToList();
+
+            if (!missingPermissions.Any())
+            {
+                return new();
+            }
+
+            var key = Guid.NewGuid().ToString();
+            var requestPermissionActivityContract = PermissionController.CreateRequestPermissionResultContract();
+            var callback = new AndroidActivityResultCallback<ISet?>(cancellationToken);
+
+            ActivityResultLauncher? launcher = null;
+            ISet? newlyGrantedPermissions = null;
+            try
+            {
+                var observer = ((ComponentActivity)_activityContext).ActivityResultRegistry;
+                launcher = observer.Register(key, requestPermissionActivityContract, callback);
+                launcher.Launch(new HashSet(missingPermissions));
+
+                newlyGrantedPermissions = await callback.Task;
+            }
+            finally
+            {
+                launcher?.Unregister();
+            }
+
+            var stillMissingPermissions = newlyGrantedPermissions is null
+                ? missingPermissions
+                : missingPermissions
+                    .Where(permission => !newlyGrantedPermissions.ToList().Contains(permission))
+                    .ToList();
+
+            if (stillMissingPermissions.Any())
+            {
+                return new()
+                {
+                    Error = RequestPermissionError.MissingPermissions,
+                    DeniedPermissions = stillMissingPermissions
+                };
+            }
+
+            return new();
+        }
+        catch (Exception e)
         {
             return new()
             {
-                Error = RequestPermissionError.MissingPermissions,
-                DeniedPermissions = stillMissingPermissions
+                ErrorException = e
             };
         }
-
-        return new();
     }
 
     //TODO:
@@ -122,7 +132,7 @@ public partial class HealthService
             new()
             {
                 HealthDataType = HealthDataType.Steps,
-                PermissionType = PermissionType.Read | PermissionType.Write
+                PermissionType = PermissionType.Read
             },
         };
 
@@ -188,47 +198,57 @@ public partial class HealthService
 
     private Result<SdkCheckError> IsSdkAvailable()
     {
-        var availabilityStatus = HealthConnectClient.GetSdkStatus(_activityContext);
-        if (availabilityStatus == HealthConnectClient.SdkUnavailable)
+        try
+        {
+            var availabilityStatus = HealthConnectClient.GetSdkStatus(_activityContext);
+            if (availabilityStatus == HealthConnectClient.SdkUnavailable)
+            {
+                return new()
+                {
+                    Error = SdkCheckError.SdkUnavailable
+                };
+            }
+
+            if (availabilityStatus == HealthConnectClient.SdkUnavailableProviderUpdateRequired)
+            {
+                string providerPackageName = "com.google.android.apps.healthdata";
+                // Optionally redirect to package installer to find a provider, for example:
+                var uriString = $"market://details?id={providerPackageName}&url=healthconnect%3A%2F%2Fonboarding";
+
+                var intent = new Intent(Intent.ActionView);
+                intent.SetPackage("com.android.vending");
+                intent.SetData(Android.Net.Uri.Parse(uriString));
+                intent.PutExtra("overlay", true);
+                intent.PutExtra("callerId", _activityContext.PackageName);
+
+                _activityContext.StartActivity(intent);
+
+                return new()
+                {
+                    Error = SdkCheckError.SdkUnavailableProviderUpdateRequired
+                };
+            }
+
+            //The Health Connect SDK supports Android 8(API level 26) or higher, while the Health Connect app is only compatible with Android 9(API level 28) or higher.
+            //This means that third-party apps can support users with Android 8, but only users with Android 9 or higher can use Health Connect.
+            //https://developer.android.com/health-and-fitness/guides/health-connect/develop/get-started#:~:text=the%20latest%20version.-,Note,-%3A%20The%20Health
+            if (!OperatingSystem.IsAndroidVersionAtLeast(26))
+            {
+                return new()
+                {
+                    Error = SdkCheckError.AndroidVersionNotSupported
+                };
+            }
+
+            return new();
+        }
+        catch (Exception e)
         {
             return new()
             {
-                Error = SdkCheckError.SdkUnavailable
+                ErrorException = e
             };
         }
-
-        if (availabilityStatus == HealthConnectClient.SdkUnavailableProviderUpdateRequired)
-        {
-            string providerPackageName = "com.google.android.apps.healthdata";
-            // Optionally redirect to package installer to find a provider, for example:
-            var uriString = $"market://details?id={providerPackageName}&url=healthconnect%3A%2F%2Fonboarding";
-
-            var intent = new Intent(Intent.ActionView);
-            intent.SetPackage("com.android.vending");
-            intent.SetData(Android.Net.Uri.Parse(uriString));
-            intent.PutExtra("overlay", true);
-            intent.PutExtra("callerId", _activityContext.PackageName);
-
-            _activityContext.StartActivity(intent);
-
-            return new()
-            {
-                Error = SdkCheckError.SdkUnavailableProviderUpdateRequired
-            };
-        }
-
-        //The Health Connect SDK supports Android 8(API level 26) or higher, while the Health Connect app is only compatible with Android 9(API level 28) or higher.
-        //This means that third-party apps can support users with Android 8, but only users with Android 9 or higher can use Health Connect.
-        //https://developer.android.com/health-and-fitness/guides/health-connect/develop/get-started#:~:text=the%20latest%20version.-,Note,-%3A%20The%20Health
-        if (!OperatingSystem.IsAndroidVersionAtLeast(26))
-        {
-            return new()
-            {
-                Error = SdkCheckError.AndroidVersionNotSupported
-            };
-        }
-
-        return new();
     }
 }
 
