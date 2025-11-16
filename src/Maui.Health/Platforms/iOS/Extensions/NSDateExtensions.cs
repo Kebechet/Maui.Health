d33dc8291ca44cb99f6b686fe1f06185de2704b2
Jan Klecka <jan.klecka12@gmail.com>
2025-11-16 17:31:13 +0100

    datetime casting fix

--- src/Maui.Health/Platforms/iOS/Extensions/NSDateExtensions.cs
+++ src/Maui.Health/Platforms/iOS/Extensions/NSDateExtensions.cs
@@ -6,7 +6,6 @@ internal static class NSDateExtensions
 {
     internal static DateTime ToDateTime(this NSDate nsDate)
     {
-        var reference = new DateTime(2001, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-        return reference.AddSeconds(nsDate.SecondsSinceReferenceDate);
+        return (DateTime)nsDate;
     }
 }
